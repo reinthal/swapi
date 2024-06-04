cf94a06b639d21b208f12f2624ea02deaf02d265
root <root@dev.kungsgatan.lan>
2024-06-04 17:10:19 +0200

    initial commit

--- swapi/assets.py
+++ swapi/assets.py
@@ -1,19 +1,33 @@
 
 import requests
 from dagster import asset, graph
+from dagster_duckdb import DuckDBResource
+import pandas as pd
 
 @asset
-def films():
+def films() -> pd.DataFrame:
     response = requests.get('https://swapi.dev/api/films/')
-    return response.json()
+    return pd.DataFrame(response.json()["results"])
 
 @asset
-def vehicles():
+def stg_films(films,  duckdb: DuckDBResource) -> None:
+    duckdb.sql("CREATE TABLE stg_films AS SELECT * FROM films")
+
+@asset
+def stg_vehicles(vehicles,  duckdb: DuckDBResource) -> None:
+    duckdb.sql("CREATE TABLE stg_vehicles AS SELECT * FROM vehicles")
+
+@asset
+def stg_people(people,  duckdb: DuckDBResource) -> None:
+    duckdb.sql("CREATE TABLE stg_people AS SELECT * FROM people")
+
+@asset
+def vehicles() -> pd.DataFrame:
     response = requests.get('https://swapi.dev/api/vehicles/')
     return response.json()
 
 @asset
-def people():
+def people() -> pd.DataFrame:
     response = requests.get('https://swapi.dev/api/people/')
     return response.json()
 
