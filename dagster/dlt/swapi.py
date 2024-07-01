876e65ae117204461d28e3d8641f072737cca983
Alexander Reinthal <email@reinthal.me>
2024-07-01 17:30:42 +0200

    clean up the code

--- dagster/dlt/swapi.py
+++ dagster/dlt/swapi.py
@@ -7,35 +7,33 @@ from typing import Any
 def swapi() -> Any:
     # Create a REST API configuration for the GitHub API
     # Use RESTAPIConfig to get autocompletion and type checking
+    base_url = "https://swapi.dev/api/"
+    
+    def _get_data(endpoint):
+        response = requests.get(base_url + endpoint)
+        response.raise_for_status()
+        return response.json()
+    
     @dlt.resource
     def films():
-        base_url = "https://swapi.dev/api/films"
-        response = requests.get(base_url)
-        response.raise_for_status()
-
-        return response.json()
+        yield _get_data("films")
 
     @dlt.resource
     def vehicles():
-        base_url = "https://swapi.dev/api/vehicles"
-        response = requests.get(base_url)
-        response.raise_for_status()
-        
-        return response.json()
+        yield _get_data("vehicles")
 
     @dlt.resource
     def people():
-        base_url = "https://swapi.dev/api/people"
-        response = requests.get(base_url)
-        response.raise_for_status()
+        yield _get_data("people")
 
-        return response.json()
-
-
-    yield films
-    yield vehicles
-    yield people
+    return films, vehicles, people
 
 
 if __name__ == "__main__":
-    pass
+
+    pipeline = dlt.pipeline(
+        pipeline_name="swapi",
+        destination="snowflake",
+        dataset_name="swapi"
+    )
+    pipeline.run()
