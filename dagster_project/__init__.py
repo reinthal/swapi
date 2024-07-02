1c8deaa8dac0671e4368d63636d7e6088c54e3c7
Alexander Reinthal <email@reinthal.me>
2024-07-02 11:41:22 +0200

    add job and schedule

--- dagster_project/__init__.py
+++ dagster_project/__init__.py
@@ -1,16 +1,16 @@
-from dagster import Definitions
+from dagster import Definitions, load_assets_from_package_module
 from dagster_project.resources import snowflake_resource, dlt_resource
-from dagster_project.assets.swapi import swapi_assets, swapi_pipeline_upstream_asset
-from dagster_project.jobs import all_assets_job, dlt_swapi_films_job
+from dagster_project.jobs import swapi_job
+from dagster_project.schedules import swapi_schedule
+from dagster_project import assets
 
-# TODO import assets from assets module using `load_assets_from_module`
-
-
+all_assets = load_assets_from_package_module(assets)
 
 # Definitions
 defs = Definitions(
-    assets=[swapi_pipeline_upstream_asset, swapi_assets],
-    jobs=[all_assets_job, dlt_swapi_films_job],
+    assets=all_assets,
+    jobs=[swapi_job],
+    schedules=[swapi_schedule],
     resources={
         "snowflake": snowflake_resource,
         "dlt": dlt_resource,
--- dagster_project/jobs.py
+++ dagster_project/jobs.py
@@ -1,11 +1,9 @@
 # TODO make schedule
 
 from dagster import define_asset_job, AssetSelection
-AssetSelection.groups("star_wars")
 
-all_assets_job = define_asset_job(name="all_assets_job", description="Loads all assets in this repository")
 
-dlt_swapi_films_job = \
+swapi_job = \
     define_asset_job(
         name="swapi_job", 
         selection=AssetSelection.groups("star_wars"),
